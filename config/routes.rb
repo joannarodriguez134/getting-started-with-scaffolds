64a85ca709e4a54d28ac85c7dfe02de6010b0466
Joanna Rodriguez <140280268+joannarodriguez134@users.noreply.github.com>
2024-02-21 18:28:31 +0000

    Using restful conventions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
+  # Routes for the Movie resource:
+
+  # CREATE
+  # post("/insert_movie", { :controller => "movies", :action => "create" })
+  post("/movies", { :controller => "movies", :action => "create" })
+          
+  # READ
+  get("/movies", { :controller => "movies", :action => "index" })
+  
+  get("/movies/:path_id", { :controller => "movies", :action => "show" })
+  
+  # UPDATE
+  
+  # post("/modify_movie/:path_id", { :controller => "movies", :action => "update" })
+  patch("/movies/:path_id", { :controller => "movies", :action => "update" })
+  
+  # DELETE
+  # get("/delete_movie/:path_id", { :controller => "movies", :action => "destroy" })
+  delete("/movies/:path_id", { :controller => "movies", :action => "destroy" })
+  #------------------------------
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
