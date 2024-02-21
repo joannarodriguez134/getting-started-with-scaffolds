e3a2fb808bac08d7d8873fee10bf38fdf3b6b7d4
Joanna Rodriguez <140280268+joannarodriguez134@users.noreply.github.com>
2024-02-21 18:32:39 +0000

    Replaced path id with just id

--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,16 @@ Rails.application.routes.draw do
   # READ
   get("/movies", { :controller => "movies", :action => "index" })
   
-  get("/movies/:path_id", { :controller => "movies", :action => "show" })
+  get("/movies/:id", { :controller => "movies", :action => "show" })
   
   # UPDATE
   
-  # post("/modify_movie/:path_id", { :controller => "movies", :action => "update" })
-  patch("/movies/:path_id", { :controller => "movies", :action => "update" })
+  # post("/modify_movie/:id", { :controller => "movies", :action => "update" })
+  patch("/movies/:id", { :controller => "movies", :action => "update" })
   
   # DELETE
-  # get("/delete_movie/:path_id", { :controller => "movies", :action => "destroy" })
-  delete("/movies/:path_id", { :controller => "movies", :action => "destroy" })
+  # get("/delete_movie/:id", { :controller => "movies", :action => "destroy" })
+  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
   #------------------------------
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
