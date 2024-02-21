e86490c50ddcff9b8750474ccd495fcdd85c1a18
Joanna Rodriguez <140280268+joannarodriguez134@users.noreply.github.com>
2024-02-21 21:13:25 +0000

    Rearranged order of paths

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 # == Schema Information
 #
-# Table name: movies
+# Table name: books
 #
 #  id          :bigint           not null, primary key
 #  description :text
@@ -9,5 +9,5 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
-class Movie < ApplicationRecord
+class Book < ApplicationRecord
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,26 @@
 Rails.application.routes.draw do
+  resources :books
   # Routes for the Movie resource:
-
+  
+  get("/movies/new", { :controller => "movies", :action => "new" })
   # CREATE
   # post("/insert_movie", { :controller => "movies", :action => "create" })
+
   post("/movies", { :controller => "movies", :action => "create" })
+
           
   # READ
   get("/movies", { :controller => "movies", :action => "index" })
   
   get("/movies/:id", { :controller => "movies", :action => "show" })
+
   
   # UPDATE
   
   # post("/modify_movie/:id", { :controller => "movies", :action => "update" })
+
+  get "/movies/:id/edit", controller: "movies", action: "edit"
+
   patch("/movies/:id", { :controller => "movies", :action => "update" })
   
   # DELETE
